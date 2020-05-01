b1c90aa01a9a21dce94eb1ab922c44c3aa07f5f6
dc <chendana95@gmail.com>
2020-05-01 07:27:13 -0400

    fixed navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,1 +1,21 @@
-export { default } from "./Nav";
+import React from 'react'
+
+function Nav() {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Google Books Search
+      </a>
+      <a className="navbar-brand" href="/search">
+        Search
+      </a>
+      <a className="navbar-brand" href="/saved">
+        Saved
+      </a>
+                
+    </nav>
+  );
+}
+
+export default Nav;
